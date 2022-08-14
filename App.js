d61886daa878cb52ad3411f857f5e5a62721b2be
leedd87 <leedav87@gmail.com>
2022-08-13 21:28:37 -0300

    tab bar created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer, TabActions } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
+import MovieScreen from "./screens/MovieScreen";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const image = {
@@ -16,20 +17,28 @@ export default function App() {
 	return (
 		<TailwindProvider>
 			<NavigationContainer>
-				<Tab.Navigator>
+				<Tab.Navigator
+					screenOptions={({ route }) => ({
+						tabBarIcon: ({ focused, color, size }) => {
+							let iconName;
+
+							if (route.name === "Home") {
+								iconName = focused ? "home" : "home-outline";
+							} else if (route.name === "Movies") {
+								iconName = focused ? "film" : "film-outline";
+							}
+							return (
+								<Ionicons name={iconName} size={size} color={color} />
+							);
+						},
+						tabBarActiveTintColor: "black",
+						tabBarInactiveTintColor: "gray",
+					})}
+				>
 					<Tab.Screen name="Home" component={HomeScreen} />
+					<Tab.Screen name="Movies" component={MovieScreen} />
 				</Tab.Navigator>
 			</NavigationContainer>
 		</TailwindProvider>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	image: {
-		flex: 1,
-		justifyContent: "center",
-	},
-});
